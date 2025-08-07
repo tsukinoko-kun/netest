a7c4f7a30beb91f3018b9990dde3555f0e9e5389
Frank Mayer <gh@frankmayer.dev>
2025-08-07 13:58:09 +0200

    feat: get data via cli

---
+++ cmd/data.go
@@ -0,0 +1,47 @@
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/tsukinoko-kun/netest/internal/db"
+	"github.com/tsukinoko-kun/netest/internal/networktest"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+type dataResponse struct {
+	TestResults []db.HistoryEntry[networktest.TestResults] `json:"testResults"`
+}
+
+var dataCmd = &cobra.Command{
+	Use:   "data",
+	Short: "Print all test data as JSON array to stdout",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		database, err := db.New()
+		if err != nil {
+			return fmt.Errorf("failed to initialize database: %w", err)
+		}
+		defer database.Close()
+
+		entries, err := db.RetrieveAll[networktest.TestResults](database)
+		if err != nil {
+			return fmt.Errorf("failed to retrieve test results: %w", err)
+		}
+
+		response := dataResponse{TestResults: entries}
+
+		encoder := json.NewEncoder(os.Stdout)
+		encoder.SetIndent("", "  ")
+		if err := encoder.Encode(response); err != nil {
+			return fmt.Errorf("failed to encode JSON: %w", err)
+		}
+
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(dataCmd)
+}
+
